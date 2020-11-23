e31c6bd4ca312a432051e711addb4c0be51d0f80
denis <denis.c1@live.fr>
2020-11-23 15:01:05 +0100

    memset corrige, cast de int c en unsigned char

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 17:04:59 by user42            #+#    #+#             */
-/*   Updated: 2020/11/22 19:39:06 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/23 14:59:43 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ void	*ft_memset(void *s, int c, size_t n)
 {
 	size_t			i;
 	unsigned char	*scpy;
+	unsigned char	ccpy;
 
 	scpy = (unsigned char*)s;
+	ccpy = (unsigned char)c;
 	i = 0;
 	while (i < n)
 	{
-		scpy[i] = c;
+		scpy[i] = ccpy;
 		i++;
 	}
 	return (s);
