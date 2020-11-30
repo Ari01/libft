b047c7a19a3294817fa33e1561b52c492b4d64a3
denis <denis.c1@live.fr>
2020-11-30 12:35:26 +0100

    annulation protection contre nul dans strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 18:27:31 by user42            #+#    #+#             */
-/*   Updated: 2020/11/30 12:31:49 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/30 12:34:56 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t i;
 
 	i = 0;
-	if (!size || !dst || !src)
+	if (!size)
 		return (ft_strlen(src));
 	while (src[i] && i < size - 1)
 	{
