5932419d7c9c02632911244545d126acc8b5cc09
denis <denis.c1@live.fr>
2020-12-02 04:20:51 +0100

    tout corrige pret a push

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 20:29:37 by user42            #+#    #+#             */
-/*   Updated: 2020/12/02 03:47:18 by user42           ###   ########.fr       */
+/*   Updated: 2020/12/02 04:14:57 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,46 +15,20 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	char	*occ;
 	size_t	i;
 	size_t	j;
-	size_t	k;
-	size_t	biglen;
 
 	i = 0;
-	j = 0;
-	biglen = ft_strlen(big);
-	if (!little[j])
+	if (!little[0])
 		return ((char *)big);
 	while (big[i] && i < len)
 	{
-		if (big[i] == little[j])
-		{
-			k = i;
-			while (big[k] && big[k] == little[j])
-			{
-				k++;
-				j++;
-			}
-			if (!little[j])
-				return (&big[i]);
-			j = 0;
-			k = 0;
-		}
-		i++;
-		/*
-		while (big[i] && big[i] != little[j] && i < len)
-			i++;
-		occ = (char *)&big[i];
-		while (big[i] && little[j] && big[i] == little[j] && i < len)
-		{
-			i++;
+		j = 0;
+		while (big[i + j] && big[i + j] == little[j] && i + j < len)
 			j++;
-		}
 		if (!little[j])
-			return (occ);
-		i = occ - big + 1;
-		j = 0;*/
+			return ((char *)&big[i]);
+		i++;
 	}
 	return (NULL);
 }
